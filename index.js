14ca4e9f6ac898217284ae01a07de4a4e3cbe744
KishorPokharel <kishorpokharel7@gmail.com>
2022-12-15 17:22:21 +0545

    testing pushing capabilities

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`App listning on ${PORT}`)
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
+app.listen(PORT, () => {
+  console.log(`App listning on ${PORT}`);
+});
