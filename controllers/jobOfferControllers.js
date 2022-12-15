f010ebbfed168c7aa0a02808939df5ba084ae8ab
samip779 <samipluitel28@gmail.com>
2022-12-16 01:22:11 +0545

    added job offer routes

--- controllers/jobOfferControllers.js
+++ controllers/jobOfferControllers.js
@@ -10,4 +10,19 @@ const addJob = async (req, res, next) => {
   res.json(result.rows[0]);
 };
 
-export { addJob };
+const getJobs = async (req, res, next) => {
+  const result = await pool.query('select * from offer');
+  res.json(result.rows);
+};
+
+const getJob = async (req, res, next) => {
+  const jobId = req.params.id;
+  const result = await pool.query('select * from offer where id=$1', [jobId]);
+  if (result.rows.length <= 0) {
+    res.status(404);
+    throw new Error('Does not exist');
+  }
+  res.send(result.rows[0]);
+};
+
+export { addJob, getJobs, getJob };
--- routes/jobOfferRoutes.js
+++ routes/jobOfferRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 
 const router = express.Router();
 
-import {addJob} from '../controllers/jobOfferControllers.js';
+import {addJob, getJobs, getJob} from '../controllers/jobOfferControllers.js';
 
 import { protect } from '../middleware/authorization.js';
 
 router.post('/',protect, addJob);
+router.get('/',protect, getJobs);
+router.get('/:id', getJob);
 
 
 export default router;
